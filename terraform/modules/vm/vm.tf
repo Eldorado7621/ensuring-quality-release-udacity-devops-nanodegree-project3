eef8e015b566bfa64add3e670b1eacfedbaf4642
Samuel Akinola <34632633+Eldorado7621@users.noreply.github.com>
2022-09-29 12:19:24 +0200

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -17,7 +17,7 @@ resource "azurerm_linux_virtual_machine" "proj3" {
   resource_group_name = "${var.resource_group}"
   size                = "Standard_B1s"
   admin_username      = "sam"
-  network_interface_ids = [azurerm_network_interface.test.id,]
+  network_interface_ids = [azurerm_network_interface.proj3.id,]
   admin_ssh_key {
     username   = "sam"
     public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDJp9XnxNOg+Uq/lYi1nGlt7NnwaEQwQRB+GP7hAb3sQyFO7+8QIblFo0xyTnLqf1oZIVnW6tJp0ChXnYnCNwZyn6CE4uXLI/BAnEFo5dg3+sJvtxeSuTzRO2NDnMC54S7dTeRx+KYaV5Q3/OQYtg95OG19lMcr7Ay3H7WWKnBIyzJwFPidlRVHN9ksTghjBO02Ao8z+rdoZy4vv/aulg/xT0TawDgo6zoC5CD9i9PC39sae2Y2gItpLyEeQFW+RS71Sa1J9kJUzw5QEwWpTDnQ/qTQyQSXyL4aQf9R5MMqPxvQI0TkTykKeJWoPMzhZzIv3havgw7WkJU7WxV3s2hlzX5E0i3AHpBsa3w7JqGwjBK5bTvIk7a6bWrUZ+cs2ceerDFL/QQXhtgIcfm95yAkqX5tbtvA+HmGDlsJcy2hjFzO+ri3rjspShGbjgc9i3Usx1XrZ2v2fV3wflPrwbeWB341shWGEY41IVDfCp/yuVniKdYodNGf80oUveQM1D0= sam@sam-HP-Pavilion-Laptop-15-eg0xxx"
